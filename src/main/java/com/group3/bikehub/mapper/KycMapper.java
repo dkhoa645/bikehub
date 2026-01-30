9b90186ad35bacd9038e1ff73fb770a21b6f1c5a
tri2k5 <trilttlk12@gmail.com>
2026-01-30 17:09:19 +0700

    Kyc implement, fixed mapstruct

--- src/main/java/com/group3/bikehub/mapper/KycMapper.java
+++ src/main/java/com/group3/bikehub/mapper/KycMapper.java
@@ -2,12 +2,14 @@ package com.group3.bikehub.mapper;
 
 import com.group3.bikehub.dto.response.KycResponse;
 import com.group3.bikehub.entity.Kyc;
+import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",
+    builder = @Builder(disableBuilder = true))
+
 public interface KycMapper {
     KycResponse toKycResponse(Kyc kyc);
     Kyc toKyc(KycResponse kycResponse);
-
 
 }
