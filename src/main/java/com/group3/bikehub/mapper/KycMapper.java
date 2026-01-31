8d0f9d8c769526700ca127ac0cca76afafde0ad8
tri2k5 <trilttlk12@gmail.com>
2026-01-31 10:45:39 +0700

    fix: handle Verify and pending Kyc from register again

--- src/main/java/com/group3/bikehub/mapper/KycMapper.java
+++ src/main/java/com/group3/bikehub/mapper/KycMapper.java
@@ -4,11 +4,13 @@ import com.group3.bikehub.dto.response.KycResponse;
 import com.group3.bikehub.entity.Kyc;
 import org.mapstruct.Builder;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 
 public interface KycMapper {
     KycResponse toKycResponse(Kyc kyc);
     Kyc toKyc(KycResponse kycResponse);
+    void updateKycFromDraft(KycResponse draft, @MappingTarget Kyc entity);
 
 }
--- src/main/java/com/group3/bikehub/repository/KycRepository.java
+++ src/main/java/com/group3/bikehub/repository/KycRepository.java
@@ -1,11 +1,18 @@
 package com.group3.bikehub.repository;
 
+import com.group3.bikehub.entity.Enum.KycStatus;
 import com.group3.bikehub.entity.Kyc;
+import com.group3.bikehub.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
-public interface KycRepository extends JpaRepository<Kyc, Long> {
-    Optional<Kyc> findByIdNumber(String id);
+public interface KycRepository extends JpaRepository<Kyc, String> {
+    boolean existsByUser(User user);
+    List<Kyc> findAll();
+    List<Kyc> findByUser(User user);
+    boolean existsByUserAndStatus(User user, KycStatus status);
+    Optional<Kyc> findByUserAndStatus(User user, KycStatus status);
 
 }
