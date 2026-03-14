e1604541d063229ad25ff0e116c0c66f1567b70b
dkhoa <dkhoa645@gmail.com>
2026-03-14 12:50:51 +0700

    feat: api delivered

--- src/main/java/com/group3/bikehub/controller/OrderController.java
+++ src/main/java/com/group3/bikehub/controller/OrderController.java
@@ -10,6 +10,7 @@ import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -31,7 +32,7 @@ public class OrderController {
                 .build();
     }
 
-    @PutMapping("/{id}/delivered")
+    @PutMapping(value ="/{id}/delivered",consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     ApiResponse<OrderResponse> deliverOrder(
             @PathVariable UUID id,
             @ModelAttribute DeliveredConfirmRequest deliveredConfirmRequest){
